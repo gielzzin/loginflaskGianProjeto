13c243d7df32f06f4443d7d1604c758f0650192f
Gabriel Henrique <gabriel.nascimento.silva22@escola.pr.gov.br>
2025-05-28 07:52:27 -0300

    Co-authored-by: OLeanrovsk2 <OLeanrovsk2@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,30 +1,61 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco_ponto.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+def hora_brasilia():
+    fuso_brasilia = pytz.timezone('America/Sao_Paulo')
+    return datetime.now(fuso_brasilia)
+
 class RegistroPonto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    hora = db.Column(db.DateTime, default=datetime.utcnow)
+    hora = db.Column(db.DateTime, default=hora_brasilia)
 
-@app.route('/', methods=['GET', 'POST'])
-def registrar_ponto():
+@app.route('/templates/entradaPonto.html', methods=['GET', 'POST'])
+def entrada_ponto():
     if request.method == 'POST':
         nome = request.form['nome']
         if nome:
             novo_registro = RegistroPonto(nome=nome)
             db.session.add(novo_registro)
             db.session.commit()
-            print(f'✅ {nome} bateu ponto às {novo_registro.hora.strftime("%H:%M:%S")}')  # Terminal
-        return redirect(url_for('registrar_ponto'))
+            print(f'✅ {nome} bateu ponto às {novo_registro.hora.strftime("%H:%M:%S")}')
+        return redirect(url_for('entrada_ponto'))
 
     registros = RegistroPonto.query.order_by(RegistroPonto.hora.desc()).all()
-    return render_template('registro.html', registros=registros)
+    return render_template('entradaPonto.html', registros=registros)
+
+@app.route('/templates/saidaPonto.html', methods=['GET', 'POST'])
+def saida_ponto():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        if nome:
+            novo_registro = RegistroPonto(nome=nome)
+            db.session.add(novo_registro)
+            db.session.commit()
+            print(f'✅ {nome} bateu ponto às {novo_registro.hora.strftime("%H:%M:%S")}')
+        return redirect(url_for('saida_ponto'))
+
+    registros = RegistroPonto.query.order_by(RegistroPonto.hora.desc()).all()
+    return render_template('entradaPonto.html', registros=registros)
+
+
+@app.route('/templates/relatorio.html', methods=['GET'])
+def relatorio():
+   if request.method == 'POST':
+        nome = request.form.get('nome')
+        RegistroPonto(nome=nome)
+
+        return redirect(url_for('relatorio'))
+   
+   registros = RegistroPonto.query.order_by(RegistroPonto.hora.desc()).all()
+   return render_template('relatorio.html', registros=registros)
 
 if __name__ == '__main__':
     with app.app_context():
--- models.py
+++ models.py
@@ -1,16 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
+
 
 db = SQLAlchemy()
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    senha = db.Column(db.String(100), nullable=False)
+    nome = db.Column(db.String(100), nullable=False) 
     registros = db.relationship('RegistroPonto', backref='usuario', lazy=True)
+
+def hora_brasilia():
+    fuso_brasilia = pytz.timezone('America/Sao_Paulo')
+    return datetime.now(fuso_brasilia)
 
 class RegistroPonto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    horario = db.Column(db.DateTime, default=datetime.utcnow)
+    horario = db.Column(db.DateTime, default=hora_brasilia)
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
