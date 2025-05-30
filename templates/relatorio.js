f846892fdb571448818fb0b561790f61fa674bc9
Gabriel Henrique <gabriel.nascimento.silva22@escola.pr.gov.br>
2025-05-30 10:42:01 -0300

    atualizacao beta

--- templates/relatorio.js
+++ templates/relatorio.js
@@ -1,20 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const tabelaEntrada = document.querySelector('.tabelaEntrada');
-    const tabelaSaida = document.querySelector('.tabelaSaida');
-  
-    document.getElementById('btnEntrada').addEventListener('click', function() {
-      if (tabelaEntrada.style.display === 'none') {
-        tabelaEntrada.style.display = 'table'; // mostra a tabela
-      } else {
-        tabelaEntrada.style.display = 'none'; // esconde a tabela
-      }
-    });
-  
-    document.getElementById('btnSaida').addEventListener('click', function() {
-      if (tabelaSaida.style.display === 'none') {
-        tabelaSaida.style.display = 'table'; // mostra a tabela
-      } else {
-        tabelaSaida.style.display = 'none'; // esconde a tabela
-      }
-    });
-  });
+
+
+function botaoEntrada() {
+  let tabelaEntrada = document.getElementById("tabelaEntrada");
+  tabelaEntrada.id('tabelaEntrada')
+  botaoEntrada.parentNode.replaceChild(botaoEntrada, botaoSaida);
+}
+function botaoSaida() {
+  let tabelaSaida = document.getElementById("tabelaSaida");
+  tabelaSaida.id('tabelaSaida')
+  botaoEntrada.parentNode.replaceChild(botaoSaida, botaoEntrada);
+}
